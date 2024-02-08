4bcffe42dfe6922cf1847c78a443745f4bac120b
50abdumalik50 <abdumalikabdukarimov50@gmail.com>
2024-02-08 16:33:56 +0600

    first commit

--- apps/products/views.py
+++ apps/products/views.py
@@ -9,3 +9,12 @@ class ProductListView(generic.ListView):
     template_name = 'index.html'
     context_object_name = "products"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['left_categories'] = Category.objects.filter(parent=None)
+        context['palto_products'] = Product.objects.filter(category__title="Пальто")[:4]
+        context['shtany_products'] = Product.objects.filter(category__title="Штаны")[:4]
+        context['crossy_products'] = Product.objects.filter(category__title="Кроссы")[:4]
+
+        return context
--- utils/image_path.py
+++ utils/image_path.py
@@ -2,10 +2,10 @@ import os
 
 
 def upload_avatar_for_user(instance, filename):
-    return f"/avatar/{instance.user.username}/{filename}"
+    return f"avatar/{instance.user.username}/{filename}"
 
 
 def upload_products(instance, filename):
-    return f"/products/{instance.product.title}/{filename}"
+    return f"products/{instance.product.title}/{filename}"
 
     # return f"/products/{instance.product.title}/{filename}"
